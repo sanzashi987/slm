8613b4f74d43f60f9d87910b65cde277740ae995
sanzashi987 <chen_ziao@126.com>
2026-04-08 00:31:43 +0800

    Refactor: implement Layer class for modular architecture and update GPT model to use Layer instances

--- gpt-2/train.py
+++ gpt-2/train.py
@@ -12,6 +12,20 @@ class GPTConfig:
     n_embd: int = 384
 
 
+class Layer(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super.__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.attn = SelfAttention(config)
+        self.mlp = MLP(config)
+
+    def forward(self, input):
+        x = x + self.attn(self.ln_1(input))
+        x = x + self.mlp(self.ln_2(input))
+        return x
+
+
 class GPT(nn.Module):
     def __init__(self, config: GPTConfig):
         super().__init__()
@@ -20,7 +34,7 @@ class GPT(nn.Module):
             {
                 "wte": nn.Embedding(config.vocab_size, config.n_embd),
                 "wpe": nn.Embedding(config.block_size, config.n_embd),
-                "h": nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                "h": nn.ModuleList([Layer(config) for _ in range(config.n_layer)]),
                 "ln_f": nn.LayerNorm(config.n_embd),  # additional layer norm
             }
         )
